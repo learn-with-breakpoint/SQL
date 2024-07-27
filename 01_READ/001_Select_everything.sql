43fc0e5338e5d9d5dc28916a564cbdf6419d4a0f
satya karthik ramachandrapurapu <40475023+caffeine-coder1@users.noreply.github.com>
2024-07-27 14:17:28 +0000

    added 2 more tables to the repo

---
+++ 01_READ/001_Select_everything.sql
@@ -0,0 +1,5 @@
+SELECT * FROM movies;
+
+-- tasks
+-- 1. select everything from passengers table
+-- 2. select everything from houses table
--- create_db.py
+++ create_db.py
@@ -2,13 +2,17 @@ import sqlite3
 import pandas as pd
 
 # Load Titanic dataset
-titanic = pd.read_csv('titanic.csv')
+titanic = pd.read_csv('data/titanic.csv')
+movie_data = pd.read_csv('data/IMDB-Movie-Data.csv')
+housing = pd.read_csv('data/USA Housing Dataset.csv')
 
 # Create an SQLite database (or connect to an existing one)
-conn = sqlite3.connect('titanic.db')
+conn = sqlite3.connect('test_db.db')
 
 # Write the DataFrame to an SQLite table
 titanic.to_sql('passengers', conn, if_exists='replace', index=False)
+movie_data.to_sql('movies', conn, if_exists='replace', index=False)
+housing.to_sql('houses', conn, if_exists='replace', index=False)
 
 # Close the connection
 conn.close()
